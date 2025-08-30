63ae7cbc5a152db54081a50116c2d0fd52bd81d9
Enrique Barcelli <kiko@barcelli.net>
2025-08-30 20:47:59 +0800

    Add update conversation

---
+++ update_conversation.py
@@ -0,0 +1,22 @@
+from config import *
+import requests
+import json
+import sys
+
+def update_conversation(conversation,metadata):
+    url = base_url+"conversations/"+conversation
+    print(metadata)
+    payload = json.dumps({
+      "metadata": json.loads(metadata)
+    })
+    headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer '+secret_key,
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text)
+
+if __name__ == '__main__':
+    update_conversation(str(sys.argv[1]),str(sys.argv[2]))
