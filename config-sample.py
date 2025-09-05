78bb591bd36bad450339256a0b13a8c88ab9889f
Enrique Barcelli <kiko@barcelli.net>
2025-09-05 23:08:50 +0800

    Change data type of vector_store to list so that multiple vector stores could be included in the file_search

--- config-sample.py
+++ config-sample.py
@@ -1,4 +1,4 @@
 base_url = "https://api.openai.com/v1/"
 secret_key = "YOUR-OPENAI-SECRET-KEY-HERE"
-vector_store = "YOUR-VECTOR-STORE-ID-HERE"
+vector_store = ["YOUR-VECTOR-STORE-IDs-HERE","SEPARATED-BY-COMMAS"]
 
